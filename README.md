54c4b2a797aef24a7f8ddc96df81a328ed0e9590
Nataniel Carrasquillo <nataniel-carras@hotmail.com>
2023-08-21 08:29:16 -0500

    Edited starter code js file and added info to readme file.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password Generator Starter Code
+
+## Description
+
+This code is used to create a password generator webpage for an employee with access to sensitive data. The password generator creates secure, randomly generated passwords based on certain criteria that is given by the user.
+
+## Website Link
+
+https://nataniel-c.github.io/password-generator
+
+## Website Screenshots
+
--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 // Assignment code here
 
 
+
+function generatePassword() {
+
+  // Criteria Selection
+
+  // Password length:
+  var pwordLength = prompt("Please enter password length. (Must be between 8 and 128)");
+  
+  // Validate input - must be a number
+  if (typeof(pwordLength) !== "number") {
+    alert("Please input how many characters you want your password to have as a number.");
+    return "Invalid password. Try again.";
+  // Validate input - must be between 8 and 128
+  } else if (pwordLength < 8) {
+    alert("Password must be 8 characters or longer.");
+    return "Invalid password. Try again.";
+  } else if (pwordLength > 128) {
+    alert("Password length can be up to 128 characters.");
+    return "Invalid password. Try again.";
+
+  // Continue to character type selection
+  } else {
+    // -Lowercase
+    var lowerCaseYN = confirm("Do you want your password to include lowercase characters?");
+    
+    // -Uppercase
+    var upperCaseYN = confirm("Do you want your password to include uppercase characters?");
+
+    // -Numeric
+    var numericYN = confirm("Do you want your password to include numeric characters?");
+
+    // -Special
+    var specialYN = confirm("Do you want your password to include special characters?");
+
+    // Validate input: at least one character type
+    if (lowerCaseYN == false && upperCaseYN == false && numericYN == false && specialYN == false) {
+      alert("Please choose at least one character type for your password.");
+      return "Invalid password. Try again."
+    } else if (lowerCaseYN == true)
+    
+
+  var generatedPassword = 
+
+
+  return generatedPassword;
+}
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +64,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
